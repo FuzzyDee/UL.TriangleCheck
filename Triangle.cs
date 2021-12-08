81d0c72daa87a78b4f5896e8f92ab65a94e283e8
FuzzyDud <dudleycowan@yahoo.com>
2021-12-08 08:28:40 +0000

    first code commit

---
+++ Triangle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace UL.TriangleCheck
+{
+    public class Triangle : TriangleBase
+    {
+
+        // Driver Code
+        public static void Main()
+        {
+
+            // Given sides of triangle
+          //  int x = 8, y = 7, z = 9;
+
+            // Function call
+          //  CheckTriangle(x, y, z);
+        }
+
+      
+    }
+}
---
+++ TriangleBase.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace UL.TriangleCheck
+{
+    public class TriangleBase
+    {
+
+        // Function to check if the triangle
+        // is equilateral or isosceles or scalene
+        public static string CheckTriangle(int x, int y, int z)
+        {
+
+            // Check for equilateral triangle
+            if (x == y && y == z)
+                return " ";
+            //  Console.WriteLine("Equilateral Triangle");
+
+            // Check for isosceles triangle
+            else if (x == y || y == z || z == x)
+                return "Isosceles Triangle";
+            //   Console.WriteLine("Isosceles Triangle");
+
+            // Otherwise scalene triangle
+            else
+            return  "Scalene Triangle";
+               // Console.WriteLine("Scalene Triangle");
+        }
+    }
+}
